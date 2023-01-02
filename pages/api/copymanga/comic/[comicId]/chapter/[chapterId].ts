867da91f1c05964fcad1fcc39a240563b6271b07
IronKinoko <kinoko_main@outlook.com>
2023-01-02 10:19:18 +0800

    refactor: move to next.js

--- pages/api/copymanga/comic/[comicId]/chapter/[chapterId].ts
+++ pages/api/copymanga/comic/[comicId]/chapter/[chapterId].ts
@@ -1,12 +1,10 @@
-import express from 'express'
-import axios from 'axios'
+import { NextApiHandler } from 'next'
+import axios from 'utils/axios'
 import { load } from 'cheerio'
-import { parseImageData } from './utils'
+import { parseImageData } from 'utils/copymanga'
 
-const router = express.Router()
-
-router.get('/comic/:comicId/chapter/:chapterId', async (req, res) => {
-  const { comicId, chapterId } = req.params as Record<string, string>
+const handler: NextApiHandler = async (req, res) => {
+  const { comicId, chapterId } = req.query as Record<string, string>
 
   const { data } = await axios.get(
     `https://www.copymanga.site/comic/${comicId}/chapter/${chapterId}`
@@ -17,6 +15,6 @@ router.get('/comic/:comicId/chapter/:chapterId', async (req, res) => {
   const jojo = data.match(/var jojo = '(.*?)'/)[1]
   const manga = parseImageData(imageData, jojo)
   res.json({ ok: true, manga })
-})
+}
 
-export default router
+export default handler
--- utils/axios.ts
+++ utils/axios.ts
@@ -4,3 +4,5 @@ axios.defaults.headers.common = {
   'user-agent':
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
 }
+
+export default axios
