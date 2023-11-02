bc18c14e64434cf857f9e2c5a55299543fa11c54
IronKinoko <kinoko_main@outlook.com>
2023-11-02 09:47:19 +0800

    feat: return next page url

--- pages/api/copymanga/comic/[comicId]/chapter/[chapterId].ts
+++ pages/api/copymanga/comic/[comicId]/chapter/[chapterId].ts
@@ -11,6 +11,7 @@ type Data = {
   ok: boolean
   message?: string
   manga?: { url: string }[]
+  next?: string
 }
 
 // http://localhost:3000/api/copymanga/comic/nvshendeairelieerzhi/chapter/685ba4c6-ad1f-11e9-ab52-00163e0ca5bd
@@ -25,8 +26,12 @@ const handler: NextApiHandler<Data> = async (req, res) => {
   const imageData = $('.imageData').attr('contentkey')!
   const jojo = data.match(/var jojo = '(.*?)'/)[1]
   const manga = parseImageData(imageData, jojo)
+  let next = $('.comicContent-next a').attr('href')
+  if (next) {
+    next = '/api/copymanga' + next
+  }
   res.setHeader('Cache-Control', 'public, max-age=86400')
-  res.json({ ok: true, manga })
+  res.json({ ok: true, manga, next })
 }
 
 export default handler
